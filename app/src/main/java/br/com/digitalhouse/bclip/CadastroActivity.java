28b706c23cdace58004f695d7fa691394f2ac885
ricardoveiga27 <ricardoveiga27@users.noreply.github.com>
2019-06-17 10:10:58 -0300

    cadastro Activity

---
+++ app/src/main/java/br/com/digitalhouse/bclip/CadastroActivity.java
@@ -0,0 +1,13 @@
+package br.com.digitalhouse.bclip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
